6d0250d0cfd6187e2b9409b8630658bdee0d3fae
VatinaCharo <njuscimenphy@gmail.com>
2021-10-05 23:53:22 +0800

    update to v1.1.0

--- src/main/java/VatinaCharo/Utils/Resources.java
+++ src/main/java/VatinaCharo/Utils/Resources.java
@@ -6,8 +6,9 @@ public class Resources {
     public static final String AUTHOR = "Vatina_Charo";
     //    public static final Long QQ = 3067710367L;
     public static final String ID = "VatinaCharo." + NAME.toLowerCase();
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
     public static final String DESCRIPTION = "小棠机器人";
+    public static final String GITHUB_URL = "https://api.github.com/repos/VatinaCharo/Malus/releases/latest";
     // Default Setting
     public static final String DEFAULT_COMMAND_PREFIX = "!!";
     public static final String DEFAULT_IMG_STORAGE_PATH = "./data/image/" + NAME + "/";
