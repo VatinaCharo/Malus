ead1536be9a02748e269e37f92f68b8917fe948b
Vatina Charo <njuscimenphy@gmail.com>
2021-10-15 17:57:26 +0800

    add command parsing todo

--- src/main/java/VatinaCharo/Commands/Parser.java
+++ src/main/java/VatinaCharo/Commands/Parser.java
@@ -4,19 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
-    private String name;
     private final Map<String, String> param = new HashMap<>();
 
-    public Parser parse(String str) {
+    public Parser parse(String[] str) throws Exception {
         // TODO: 2021/10/14 实现比较复杂的命令解析
+        // -l nju -info "this is a info" -out "这是输出，这是输出。"
+        // =>
+        // l nju
+        // info this is a info
+        // out 这是输出，这是输出。
+        // 可能的非法输入
+        // - l nju
+        // -
+        // -l
         return this;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public Map<String, String> getParam() {
         return param;
+    }
+
+    private boolean isParam(String s) {
+        return s.startsWith("-") && s.length() != 1;
     }
 }
